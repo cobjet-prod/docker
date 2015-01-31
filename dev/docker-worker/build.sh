9a4a2bae0808b5065bfe24a805b606d8becf44d9
docker <docker@cobjet.com>
2015-01-31 01:12:51 -0800

    updated

--- dev/docker-worker/build.sh
+++ dev/docker-worker/build.sh
@@ -1,4 +1,5 @@
 docker build -t cobjet/worker .
 
 
-echo "docker run --privileged -t -i -p 8888:8888 -p 4430:4430 cobjet/worker /bin/bash"
+#echo "docker run --privileged -t -i -p 8888:8888 -p 4430:4430 cobjet/worker /bin/bash"
+echo "docker run -it -v /var/run/docker.sock:/var/run/docker.sock -p 8888:8888 -p 4430:4430 cobjet/worker /bin/bash"
